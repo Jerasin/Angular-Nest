1fc1d942a3adb75659f556485cc06ceffd0399c5
Jerasin <jinyonglover12@gmail.com>
2022-04-24 19:44:02 +0700

    add fire base

--- libs/users/src/controllers/users.controller.ts
+++ libs/users/src/controllers/users.controller.ts
@@ -1,6 +1,7 @@
 import { Controller,Get } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import {Message} from '@web/api-interfaces'
+
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {
--- libs/users/src/services/users.service.ts
+++ libs/users/src/services/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import {Message} from '@web/api-interfaces'
+
 @Injectable()
 export class UsersService {
     getData(): Message {
