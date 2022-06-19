66224256233016809a53e0abe45f672ef2ce2dfc
jerasin-ch <jerasin.ch@centrilliontech.co.th>
2022-06-19 17:45:54 +0700

    fix(main): change config jest

--- libs/users/src/users.test.module.ts
+++ libs/users/src/users.test.module.ts
@@ -1,10 +1,11 @@
 import { Test } from '@nestjs/testing';
-import {UsersService} from './services/users.service'
-import {UsersController} from './controllers/users.controller'
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
+import { FirebaseService } from '@web/firebase';
 
-export const UserTestModule = async ()=>{
-  return  await Test.createTestingModule({
-        controllers: [UsersController],
-        providers: [UsersService],
-      }).compile();
-}
+export const UserTestModule = async () => {
+  return await Test.createTestingModule({
+    controllers: [UsersController],
+    providers: [UsersService, FirebaseService],
+  }).compile();
+};
